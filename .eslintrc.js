3bd7c3590327c594991566492788d21e61f488c4
Dimitri <multigamesfry@gmail.com>
2018-02-16 16:30:49 +0100

    add faker generator

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,10 +18,11 @@ module.exports = {
   // add your custom rules here
   rules: {
     // allow async-await
-    'generator-star-spacing': 'off',
+	  'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-	'indent': ['error', 'tab'],
-	'no-tabs': 0
+	  'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	  'indent': ['error', 'tab'],
+	  'no-tabs': 0,
+	  'one-var': 0
   }
 }
